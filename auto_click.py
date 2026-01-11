abf325b0a67b07fe5f0d539cc0c0fb0acd35b7b2
Han zhiqiang <hzq8710@gmail.com>
2026-01-11 17:06:57 +0800

    Update auto_click.py

--- auto_click.py
+++ auto_click.py
@@ -56,7 +56,7 @@ clean_old_logs()
 
 # 主逻辑开始
 try:
-    driver.get("https://super-appapppy-lover.streamlit.app/")
+    driver.get("https://xsll111.streamlit.app/")
     print("已打开网页，等待页面加载 30 秒...")
     time.sleep(30)  # 初次加载等待
 
